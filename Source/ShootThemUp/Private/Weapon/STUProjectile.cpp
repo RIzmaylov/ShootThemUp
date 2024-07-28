48e15c7856fe7015bfaef830688ecc1fe76cb858
Ruslan <izya.man18@gmail.com>
2024-07-28 18:29:15 +0300

    5.15 Launcher apply radial damage

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -3,13 +3,17 @@
 #include "Weapon/STUProjectile.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 ASTUProjectile::ASTUProjectile()
 {
- 	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 
 	CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
 	CollisionComponent->InitSphereRadius(5.0f);
+	CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
 	SetRootComponent(CollisionComponent);
 
 	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
@@ -22,7 +26,40 @@ void ASTUProjectile::BeginPlay()
 	Super::BeginPlay();
 
 	check(MovementComponent);
+	check(CollisionComponent);
+
 	MovementComponent->Velocity = ShotDirection * MovementComponent->InitialSpeed;
-	SetLifeSpan(5.0f);
+	CollisionComponent->IgnoreActorWhenMoving(GetOwner(), true);
+	CollisionComponent->OnComponentHit.AddDynamic(this, &ASTUProjectile::OnProjectileHit);
+	SetLifeSpan(LifeSeconds);
+}
+
+void ASTUProjectile::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!GetWorld()) return;
+
+	MovementComponent->StopMovementImmediately();
+
+	// make damage
+	UGameplayStatics::ApplyRadialDamage(GetWorld(), //
+		DamageAmount,								//
+		GetActorLocation(),							//
+		DamageRadius,								//
+		UDamageType::StaticClass(),					//
+		{GetOwner()},								//
+		this,										//
+		GetController(),							//
+		DoFullDamage);
+
+	DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 24, FColor::Red, false, 5.0f);
+
+	Destroy();
+}
+
+AController* ASTUProjectile::GetController() const
+{
+	const auto Pawn = Cast<APawn>(GetOwner());
+	return Pawn ? Pawn->GetController() : nullptr;
 }
 
